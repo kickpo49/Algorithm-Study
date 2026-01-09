a18033c30be980e2a110cdd01cc62fc7310924a2
Declan Chung <48705164+kickpo49@users.noreply.github.com>
2026-01-09 12:10:18 +0900

    [level 0] Title: 정수 부분, Time: 0.00 ms, Memory: 9.18 MB -BaekjoonHub

---
+++ 프로그래머스/0/181850. 정수 부분/정수 부분.py
@@ -0,0 +1,18 @@
+# Python3
+def solution(flo):
+    answer = int(flo)
+    return answer
+
+# JavaScript
+# function solution(flo) {
+#     // Math.trunc()는 소수점 이하를 제거합니다.
+#     return Math.trunc(flo);
+# }
+
+# Java
+# class Solution {
+#     public int solution(double flo) {
+#         // (int) 캐스팅을 통해 소수점 이하를 버립니다.
+#         return (int) flo;
+#     }
+# }
