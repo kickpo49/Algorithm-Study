20f290c52144e6af2d051c788e16085a61735089
Declan Chung <48705164+kickpo49@users.noreply.github.com>
2026-01-19 12:33:38 +0900

    [level 0] Title: 문자열로 변환, Time: 0.00 ms, Memory: 9.32 MB -BaekjoonHub

---
+++ 프로그래머스/0/181845. 문자열로 변환/문자열로 변환.py
@@ -0,0 +1,17 @@
+# Python3
+def solution(n):
+    return str(n)
+
+# JavaScript
+# function solution(n) {
+#     return String(n);
+#     // 또는 return n.toString();
+# }
+
+# Java
+# class Solution {
+#     public String solution(int n) {
+#         return String.valueOf(n);
+#         // 또는 return Integer.toString(n);
+#     }
+# }
