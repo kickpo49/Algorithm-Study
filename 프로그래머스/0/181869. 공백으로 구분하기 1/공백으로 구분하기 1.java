01cfc2e57659ebb1d2460a25795e4c7ee4eb5261
Declan Chung <48705164+kickpo49@users.noreply.github.com>
2026-02-09 22:19:40 +0900

    [level 0] Title: 공백으로 구분하기 1, Time: 0.23 ms, Memory: 74.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String[] solution(String my_string) {
+        String[] answer = my_string.split(" ");
+        return answer;
+    }
+}
+
+// class Solution {
+//     public String[] solution(String my_string) {
+//         // trim()은 앞뒤 공백 제거
+//         String[] answer = my_string.trim().split(" ");
+        
+//         return answer;
+//     }
+// }
